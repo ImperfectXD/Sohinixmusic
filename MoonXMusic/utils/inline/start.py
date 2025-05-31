36c90d0db6cbb9d6496b3d3af56c9fd8d0d5e085
𓆩 𝐒 𝐇 𝐔 𝐁 𝐇 𝐎 𓆪 <shubhendugarai917@gmail.com>
2025-05-31 19:55:03 +0530

    Update start.py

--- MoonXMusic/utils/inline/start.py
+++ MoonXMusic/utils/inline/start.py
@@ -13,7 +13,7 @@ def start_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text="•𝐒ᴜʙsᴄʀɪʙᴇ 𝐓ᴏ 𝐒ᴏʜɪɴɪ•",url=f"https://t.me/About_Sohini"),
+            InlineKeyboardButton(text="•𝐒ᴜʙsᴄʀɪʙᴇ 𝐓ᴏ 𝐑ᴀᴊ•",url=f"https://t.me/godytv3"),
         ],
     ]
     return buttons
@@ -28,7 +28,7 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text="˹𝐒ᴜʙsᴄʀɪʙᴇ ᴛᴏ sᴏʜɪɴɪ˼",url=f"https://t.me/About_Sohini"),
+            InlineKeyboardButton(text="˹𝐒ᴜʙsᴄʀɪʙᴇ ᴛᴏ 𝐑ᴀᴊ˼",url=f"https://t.me/godytv3"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
